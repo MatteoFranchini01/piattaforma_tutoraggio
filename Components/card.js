fbcf4b60e1147788b5890a3a71381c7bd210482a
deborabini <155816668+deborabini@users.noreply.github.com>
2024-04-13 20:49:48 +0200

    Add files via upload

---
+++ Components/card.js
@@ -0,0 +1,20 @@
+import {Component} from "react";
+
+class Card extends Component{
+    render(){
+        return(
+            <div className='col' style={{margin:'10px'}}>
+                <div className="card" style={{width: '15rem', textAlign: 'center'}}>
+                    <img src={this.props.immagine} className="card-img-top"  />
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.nome}</h5>
+                            <p className="card-text">Da €{this.props.prezzo}</p>
+                            <a href="#" className="btn btn-primary">Cerca insegnanti</a>
+                        </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
