53ac514cdbdf5e88cef64c72760f02dcaae0982f
Matteo Franchini <matteo.franchini4@studenti.unipr.it>
2024-04-15 16:21:08 +0200

    server come classe

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
 const express = require('express')
-const app = express()
 
-const cards = [
-    {id: 0, nome: "Matematica", prezzo: 20},
-    {id: 1, nome: "Fisica", prezzo: 25},
-    {id: 2, nome: "Chimica", prezzo: 25},
-    {id: 3, nome: "Algebra e geometria", prezzo: 20},
-    {id: 4, nome: "Algoritmi e strutture dati", prezzo: 20},
-    {id: 5, nome: "FCA", prezzo: 35},
-    {id: 6, nome: "Statistica e probabilità", prezzo: 25},
-    {id: 7, nome: "Telecomunicazioni", prezzo: 15},
-]
+class Server {
+    constructor() {
+        this.app = express()
+        this.cards = [
+            {id: 0, nome: "Matematica", prezzo: 20},
+            {id: 1, nome: "Fisica", prezzo: 25},
+            {id: 2, nome: "Chimica", prezzo: 25},
+            {id: 3, nome: "Algebra e geometria", prezzo: 20},
+            {id: 4, nome: "Algoritmi e strutture dati", prezzo: 20},
+            {id: 5, nome: "FCA", prezzo: 35},
+            {id: 6, nome: "Statistica e probabilità", prezzo: 25},
+            {id: 7, nome: "Telecomunicazioni", prezzo: 15},
+        ]
+        this.port = process.env.PORT || 3000;
+    }
 
-app.get('/cards', (req, res) => {
-    res.json(cards);
-})
+    start() {
+        this.app.get("/cards", (req, res) => {
+            res.json(this.cards);
+        });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listen to port: ${PORT}`))
+        this.app.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}`)
+        });
+    }
+}
+
+const server = new Server();
+server.start();
