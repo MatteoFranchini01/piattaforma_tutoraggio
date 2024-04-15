875c6594dd7e2aceb0f4de816cb4c860f37f247e
Matteo Franchini <matteo.franchini4@studenti.unipr.it>
2024-04-15 16:04:36 +0200

    Integrazione client/server riuscita... chiusura della branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,25 @@
 import Navbar from "./Components/navbar";
 import Card from "./Components/card";
 import {Component, useEffect, useState} from "react";
+import axios from 'axios';
 
 import data from "bootstrap/js/src/dom/data";
 
 
-function App_fun() {
-    const [backendData, setBackendData] = useState([{}])
-
-    useEffect(() => {
-        fetch("/api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
-    }, []);
-}
 class App extends Component {
-    subjects = {
-        /*da leggere dal databse*/
-        cards: [
-            {id: 0, nome: "Matematica", prezzo: 20},
-            {id: 1, nome: "Fisica", prezzo: 25},
-            {id: 2, nome: "Chimica", prezzo: 25},
-            {id: 3, nome: "Algebra e geometria", prezzo: 20},
-            {id: 4, nome: "Algoritmi e strutture dati", prezzo: 20},
-            {id: 5, nome: "FCA", prezzo: 35},
-            {id: 6, nome: "Statistica e probabilità", prezzo: 25},
-            {id: 7, nome: "Telecomunicazioni", prezzo: 15},
-        ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: []
+        };
     }
+
+    async componentDidMount() {
+        const response = await fetch('/cards');
+        const data = await response.json();
+        this.setState({ cards: data });
+    }
+
     render() {
         return (
             <>
@@ -40,7 +28,7 @@ class App extends Component {
                     <h1>Title</h1>
                     <hr/>
                     <div className='row'>
-                        {this.subjects.cards.map((card, index) => (
+                        {this.state.cards.map((card, index) => (
                             <Card
                                 key={card.id}
                                 nome={card.nome}
@@ -48,15 +36,6 @@ class App extends Component {
                             />
                         ))}
                     </div>
-                </div>
-                <div>
-                    {(typeof backendData.users === 'undefined') ? (
-                        <p>Loading...</p>
-                    ) : (
-                        backendData.users.map((user, index) => (
-                            <p key={i}>{user}</p>
-                        ))
-                    )}
                 </div>
             </>
         );
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+const cards = [
+    {id: 0, nome: "Matematica", prezzo: 20},
+    {id: 1, nome: "Fisica", prezzo: 25},
+    {id: 2, nome: "Chimica", prezzo: 25},
+    {id: 3, nome: "Algebra e geometria", prezzo: 20},
+    {id: 4, nome: "Algoritmi e strutture dati", prezzo: 20},
+    {id: 5, nome: "FCA", prezzo: 35},
+    {id: 6, nome: "Statistica e probabilità", prezzo: 25},
+    {id: 7, nome: "Telecomunicazioni", prezzo: 15},
+]
+
+app.get('/cards', (req, res) => {
+    res.json(cards);
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listen to port: ${PORT}`))
