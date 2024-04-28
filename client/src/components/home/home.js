c4de46fe5d2a1dfb35a7feb7a2c82dc12f11b9f2
deborabini <debora.bini1@studenti.unipr.it>
2024-04-28 22:20:50 +0200

    paragrafi home page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -16,8 +16,10 @@ export default class Home extends React.Component{
                     <div className="box presentation-box">
                         <h2 className="title">KnowHow: il tuo partner nell'apprendimento!</h2>
                         <p className="presentation-paragraph">
-                            KnowHow è qui per accompagnare ogni studente nel raggiungimento dei propri obiettivi scolastici.
-                            Grazie al nostro servizio di ripetizioni online, potrai prendere lezioni private da qualsiasi parte del mondo!
+                            KnowHow è qui per accompagnare ogni studente nel raggiungimento dei propri obiettivi
+                            scolastici.
+                            Grazie al nostro servizio di ripetizioni online, potrai prendere lezioni private da
+                            qualsiasi parte del mondo!
                             <br/> <br/>
                             <i className="fa fa-check" aria-hidden="true"> Ripetizioni private online</i> <br/>
                             <i className="fa fa-check" aria-hidden="true"> Insegnanti qualificati</i> <br/>
@@ -44,20 +46,26 @@ export default class Home extends React.Component{
 
                     </div>
                 </section>
-
+                <hr className="centered-hr"/>
                 <section className="basic">
-                    <h2 className="h2-title">come funziona</h2>
+                    <h2 className="h2-title">Come funziona</h2>
                     <div className="first">
                         <h4 className="basic-titles online"> Ripetizioni individuali online</h4>
-                        <p className="basic-paragraph"> Le ripetizioni online individuali offrono un'apprendimento personalizzato, flessibile e interattivo.
-                            Gli studenti ricevono attenzione individuale dagli insegnanti, possono pianificare le lezioni secondo i propri orari e utilizzare strumenti tecnologici avanzati per un apprendimento coinvolgente.
-                            Questo approccio mirato massimizza il potenziale degli studenti e migliora i risultati accademici in modo efficiente. </p>
+                        <p className="basic-paragraph"> Le ripetizioni online individuali offrono un'apprendimento
+                            personalizzato, flessibile e interattivo.
+                            Gli studenti ricevono attenzione individuale dagli insegnanti, possono pianificare le
+                            lezioni secondo i propri orari e utilizzare strumenti tecnologici avanzati per un
+                            apprendimento coinvolgente.
+                            Questo approccio mirato massimizza il potenziale degli studenti e migliora i risultati
+                            accademici in modo efficiente. </p>
                     </div>
                     <div className="box-container">
                         <div className="box second-box">
                             <h4 className="basic-titles grade"> Per ogni scuola ed età</h4>
-                            <p className="basic-paragraph">Il nostro servizio di ripetizioni online è adatto a ogni scuola ed età.
-                                <br/>Con insegnanti qualificati, offriamo supporto personalizzato per garantire il successo accademico di ogni studente.</p>
+                            <p className="basic-paragraph">Il nostro servizio di ripetizioni online è adatto a ogni
+                                scuola ed età.
+                                <br/>Con insegnanti qualificati, offriamo supporto personalizzato per garantire il
+                                successo accademico di ogni studente.</p>
                         </div>
                         <div className="box third-box">
                             <h4 className="basic-titles teachers"> Gli insegnanti</h4>
@@ -69,13 +77,12 @@ export default class Home extends React.Component{
                         </div>
                     </div>
                 </section>
-
                 <section className="section section-primary howToStartSection">
                     <div className="section-content">
                         <div className="container white-color">
                             <div className="row mb-3">
                                 <div className="col-12">
-                                    <h2 className="h2-title">come iniziare</h2>
+                                    <h2 className="h2-title">Come iniziare</h2>
                                 </div>
                             </div>
                             <div className="row">
@@ -99,9 +106,8 @@ export default class Home extends React.Component{
                         </div>
                     </div>
                 </section>
-
                 <section className="cards">
-                    <h2 className="h2-title">le materie richieste</h2>
+                    <h2 className="h2-title">Le materie richieste</h2>
                     <div className="row p-5">
                         <Card></Card>
                         <Card></Card>
@@ -110,9 +116,9 @@ export default class Home extends React.Component{
                         <Card></Card>
                     </div>
                 </section>
-
+                <hr className="centered-hr"/>
                 <section className="questions">
-                    <h2 className="h2-title">le domande frequenti</h2>
+                    <h2 className="h2-title">Le domande frequenti</h2>
                     <div className="accordion" id="accordionExample">
                         <Accordion id={1} title={"Domanda 1"} answer={"risposta 1"}/>
                         <Accordion id={2} title={"Domanda 2"} answer={"risposta 2"}/>
