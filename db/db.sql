a44c4c2bd0a17dac96e51c4cf9e74dd3d6e54f22
Matteo Franchini <matteo.franchini4@studenti.unipr.it>
2024-05-24 16:11:14 +0200

    db aggiornato e popolato

--- db/db.sql
+++ db/db.sql
@@ -1,69 +1,56 @@
-create table Discente
+CREATE TABLE RUOLI
 (
-    ID_DISCENTE int auto_increment
-        primary key,
-    NOME        varchar(200) not null,
-    COGNOME     varchar(200) not null,
-    ID          int  null,
-    constraint Discente_Utenti_ID_fk
-        foreign key (ID) references Utenti (ID)
-);
-
-create table Lezioni
-(
-    ID_LEZ      int auto_increment
-        primary key,
-    ID_TUTOR    int null,
-    ID_DISCENTE int null,
-    ID_MATERIA  int null,
-    constraint Lezioni_Discente_ID_DISCENTE_fk
-        foreign key (ID_DISCENTE) references Discente (ID_DISCENTE),
-    constraint Lezioni_Materie_ID_fk
-        foreign key (ID_MATERIA) references Materie (ID),
-    constraint Lezioni_Tutor_ID_TUTOR_fk
-        foreign key (ID_TUTOR) references Tutor (ID_TUTOR)
-);
-
-create table Materie
-(
-    ID       int auto_increment
-        primary key,
-    NOME     varchar(200) not null,
-    TUTOR_ID int  null,
-    PREZZO   int  null,
-    constraint Materie_Tutor_ID_TUTOR_fk
-        foreign key (TUTOR_ID) references Tutor (ID_TUTOR)
-);
-
-create table RUOLI
-(
-    ID         int  not null
-        primary key,
-    RUOLO_DESC varchar(200) not null
+    ID SERIAL PRIMARY KEY,
+    RUOLO_DESC VARCHAR(200) NOT NULL
 );
 
 INSERT INTO RUOLI (ID, RUOLO_DESC) VALUES (1, 'root'), (2, 'tutor'), (3, 'user');
 
-create table Tutor
+CREATE TABLE Utenti
 (
-    ID       int  null,
-    NOME     char null,
-    COGNOME  char null,
-    MAIL     char null,
-    ID_TUTOR int  not null
-        primary key,
-    constraint Tutor_Utenti_ID_fk
-        foreign key (ID) references Utenti (ID)
+    ID SERIAL PRIMARY KEY,
+    NOME VARCHAR(200) NOT NULL,
+    COGNOME VARCHAR(200) NOT NULL,
+    PASSWORD VARCHAR(255) NULL,
+    PRIVILEGI INT NOT NULL,
+    CONSTRAINT Utenti_RUOLI_ID_fk FOREIGN KEY (PRIVILEGI) REFERENCES RUOLI (ID)
 );
 
-create table Utenti
+CREATE TABLE Tutor
 (
-    ID        int auto_increment
-        primary key,
-    NOME      char       not null,
-    COGNOME   char       not null,
-    PASSWORD  varchar(1) null,
-    PRIVILEGI int        not null,
-    constraint Utenti_RUOLI_ID_fk
-        foreign key (PRIVILEGI) references RUOLI (ID)
+    ID INT NULL,
+    NOME VARCHAR(200) NULL,
+    COGNOME VARCHAR(200) NULL,
+    MAIL VARCHAR(200) NULL,
+    ID_TUTOR SERIAL PRIMARY KEY,
+    CONSTRAINT Tutor_Utenti_ID_fk FOREIGN KEY (ID) REFERENCES Utenti (ID)
+);
+
+CREATE TABLE Discente
+(
+    ID_DISCENTE SERIAL PRIMARY KEY,
+    NOME VARCHAR(200) NOT NULL,
+    COGNOME VARCHAR(200) NOT NULL,
+    ID INT NULL,
+    CONSTRAINT Discente_Utenti_ID_fk FOREIGN KEY (ID) REFERENCES Utenti (ID)
+);
+
+CREATE TABLE Materie
+(
+    ID SERIAL PRIMARY KEY,
+    NOME VARCHAR(200) NOT NULL,
+    TUTOR_ID INT NULL,
+    PREZZO INT NULL,
+    CONSTRAINT Materie_Tutor_ID_TUTOR_fk FOREIGN KEY (TUTOR_ID) REFERENCES Tutor (ID_TUTOR)
+);
+
+CREATE TABLE Lezioni
+(
+    ID_LEZ SERIAL PRIMARY KEY,
+    ID_TUTOR INT NULL,
+    ID_DISCENTE INT NULL,
+    ID_MATERIA INT NULL,
+    CONSTRAINT Lezioni_Discente_ID_DISCENTE_fk FOREIGN KEY (ID_DISCENTE) REFERENCES Discente (ID_DISCENTE),
+    CONSTRAINT Lezioni_Materie_ID_fk FOREIGN KEY (ID_MATERIA) REFERENCES Materie (ID),
+    CONSTRAINT Lezioni_Tutor_ID_TUTOR_fk FOREIGN KEY (ID_TUTOR) REFERENCES Tutor (ID_TUTOR)
 );
