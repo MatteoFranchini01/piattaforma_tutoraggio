dd11d98741e13bcb2d312fcb5615dbfb1100478e
deborabini <debora.bini1@studenti.unipr.it>
2024-06-19 23:02:23 +0200

    Sezione filters in teachers.js

--- client/src/components/pages/teachers.js
+++ client/src/components/pages/teachers.js
@@ -1,23 +1,34 @@
-import React, {Component} from "react";
-import "../../css/teachers.css"
+import React, {useEffect, useState} from "react";
+import "../../css/teachers.css";
 import TeachersCard from "../mainLayout/template/teachersCard";
 
-// TODO: da passare l'id della materia alla funzione per avere tutti gli insegnanti correlati a quella materia
+//TODO a questa funzione deve arrivare l'ID della materia, in modo da sapere quali sono e quanti sono gli insegnanti che fanno quella materia
 export default function Teacher() {
+    const [numberOfTeachers, setNumberOfTeachers] = useState(6);
+    const [componentsArray, setComponentsArray] = useState([]);
+    const [price, setPrice] = useState(50);
 
-    //TODO calcolare quanti insegnanti sono presenti relativi a quella materia
-    const numberOfTeachers = 6;
-    const componentsArray = Array.from({ length: numberOfTeachers }, (_, i) => i);
+    var subjectSelected = "Materia Selezionata"
 
-    // ordinare insegnanti per
-    // - prezzo
-    // - stelline
-    // - livello [tutti - primaria - secondaria - superiori - università]
+    useEffect(() => {
+        const componentsArray = Array.from({ length: numberOfTeachers }, (_, i) => i);
+        setComponentsArray(componentsArray);
+    }, [numberOfTeachers]);
 
-    return(
+    const handlePriceChange = (event) => {
+        setPrice(event.target.value);
+    };
+
+    return (
         <>
             <div className="main-content">
                 <div className="box filters">
+                    <h3 className="title-teachers-filters">Personalizza la tua ricerca!</h3>
+
+                    <input className="form-control" type="text" placeholder="Default input"
+                           aria-label="default input example" readOnly value={subjectSelected}
+                           onChange={handlePriceChange}/>
+
                     <select className="form-select levels">
                         <option selected>Scegli il livello</option>
                         <option value="tutti">Tutti i livelli</option>
@@ -25,11 +36,24 @@ export default function Teacher() {
                         <option value="medie">Scuola secondaria</option>
                         <option value="superiori">Scuola superiore</option>
                     </select>
+
+                    <label htmlFor="customRange1" className="form-label">
+                        Scegli il prezzo massimo: {price}.00€
+                    </label>
+                    <input
+                        type="range"
+                        className="form-range"
+                        id="customRange1"
+                        value={price}
+                        onChange={handlePriceChange}
+                    />
+
+                    <button className="btn btn-primary find" type="submit">Cerca</button>
                 </div>
                 <div className="vr vr-teach"></div>
                 <div className="box row teachers-information">
                     {componentsArray.map((index) => (
-                        <TeachersCard/>
+                        <TeachersCard key={index}/>
                     ))}
                 </div>
             </div>
