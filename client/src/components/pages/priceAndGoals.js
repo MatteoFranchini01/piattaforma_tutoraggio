f437c479a737f14e8404b1533f63eda1ff297ee2
deborabini <debora.bini1@studenti.unipr.it>
2024-08-08 18:36:32 +0200

    Finita priceAndGoals.js

--- client/src/components/pages/priceAndGoals.js
+++ client/src/components/pages/priceAndGoals.js
@@ -9,16 +9,24 @@ export default function PriceAndGoals(){
                 <h2 className="h2-title-price-goals">Prezzi e obiettivi</h2>
                 <div className="box-container price-goals">
                     <div className="box prices-goals first-box-price">
-                        <h4 className="basic-titles uno">Titolo 1</h4>
-                        <p className="basic-paragraph-prices-goals"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
+                        <h4 className="basic-titles uno">Obbiettivi</h4>
+                        <p className="basic-paragraph-prices-goals">
+                            L'obbiettivo di KnowHow è quello di mettere in contatto studenti e tutor.
+                            Il servizio permette ad entrambi le parti interessate la possibilità di scambiarsi un indirizzo email per comunicare tra loro.
+                            KowHow non offre un servizio di messaggistica integrato, nè una piattaforma utile per le video conferenze.
+                            Di conseguenza, spetta a coloro che sono coinvolti la responsabilità di accordarsi, privatamente, su quale piattaforma utilizzare per svolgere la lezione.
+                            All'interno del sito è possibile visionare i propri impegni: ai tutor viene mostrato l'elenco degli orari prenotati dagli studenti; mentre agli studenti viene data la possibilità di visionare quali lezioni hanno prenotato.
+
+                        </p>
                     </div>
                     <div className="box prices-goals second-box-price">
-                        <h4 className="basic-titles due">Titolo 2</h4>
-                        <p className="basic-paragraph-prices-goals">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                    <div className="box prices-goals third-box-price">
-                        <h4 className="basic-titles tre">Titolo 3</h4>
-                        <p className="basic-paragraph-prices-goals">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        <h4 className="basic-titles due">Prezzi</h4>
+                        <p className="basic-paragraph-prices-goals">
+                            Ogni tutor ha la facoltà di stabilire il prezzo per ciascuna lezione.
+                            Si suggerisce di determinare tale importo tenendo conto della materia trattata e del livello di esperienza del tutor. <br/>
+                            KnowHow non fornisce alcun servizio di pagamento integrato e non si assume responsabilità nel caso in cui gli accordi tra le parti non vengano rispettati.
+                            È responsabilità degli studenti, e nel caso siano minorenni, delle loro famiglie, effettuare i pagamenti agli insegnanti in modo tempestivo.
+                        </p>
                     </div>
                 </div>
             </div>
