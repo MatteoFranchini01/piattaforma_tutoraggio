1a96ae8044d7f5b12d52d68ef64f60911567323f
deborabini <debora.bini1@studenti.unipr.it>
2024-08-16 16:53:46 +0200

    Sistemazione bio

--- client/src/components/pages/tutor.js
+++ client/src/components/pages/tutor.js
@@ -12,20 +12,13 @@ export default function Tutor() {
     const [availability, setAvailability] = React.useState([]);
     const [bookedUp, setBookedUp] = React.useState([]);
     const [tutorInfo, setTutorInfo] = React.useState([]);
-    const [bio, setBio] = React.useState('');
 
     const tutorRating = 4;
     const numberOfReviews = 27
 
-    /*const bio= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    */
     React.useEffect(() => {
         getInfoTutor();
-    }, []); // Usa [] per eseguire solo al montaggio
+    }, []);
 
     function getInfoTutor() {
         const url = `http://localhost:3000/teachers/${subject_name}/${tutor_id}`;
@@ -44,7 +37,6 @@ export default function Tutor() {
                 }));
                 console.log("Mapped data:", temp);
                 setTutorInfo(temp);
-                setBio(temp.bio);
             })
             .catch(error => {
                 console.error('Error fetching data', error);
@@ -57,7 +49,6 @@ export default function Tutor() {
         setSelectedDay(day);
     };
 
-    // Verifica se tutorInfo ha dati e se è un array con almeno un elemento
     const hasTutorInfo = tutorInfo.length > 0;
 
     return (
@@ -104,7 +95,7 @@ export default function Tutor() {
                     <hr className="centered-hr"/>
                     <div className="tutor-competences">
                         <h4 className="tutor-title-bio">Descrizione</h4>
-                        <p className="tutor-bio">{bio}</p>
+                        <p className="tutor-bio">{tutorInfo[0]?.bio || "Bio non disponibile"}</p>
                     </div>
                     <hr className="centered-hr"/>
                     <div className="tutor-reservation">
